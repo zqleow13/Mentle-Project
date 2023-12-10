ae50bb6d0886ce7ee19fa27857a5a5925f9eed83
zqleow13 <hayleyleow13@gmail.com>
2023-12-10 20:09:12 +0800

    Separate db seeding and server files

---
+++ backend/src/seed.js
@@ -0,0 +1,71 @@
+import sqlite from 'sqlite3';
+import csv from 'fast-csv';
+import fs from 'fs';
+
+const sqlite3 = sqlite.verbose();
+
+// Create SQLite database and connect to db
+const db = new sqlite3.Database('resources.db', (err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.message);
+    } else {
+        console.log('Connected to the database');
+    }
+});
+
+// Create resources table
+db.run('CREATE TABLE IF NOT EXISTS resources (ID INTEGER PRIMARY KEY, Name TEXT UNIQUE, Organisation TEXT, Type TEXT, Audience TEXT, Location TEXT, Free TEXT, Referral TEXT)', (err) => {
+    if (err) {
+        console.error('Error creating table:', err.message);
+    } else {
+        console.log('Table created successfully');
+    }
+});
+
+// Import data from CSV to SQLite
+const csvFilePath = 'resources.csv';
+
+const stream = fs.createReadStream(csvFilePath);
+
+const processData = (data) => {
+    return new Promise((resolve, reject) => {
+        db.run('INSERT INTO resources (ID, Name, Organisation, Type, Audience, Location, Free, Referral) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [data.ID, data.Name, data.Organisation, data.Type, data.Audience, data.Location, data.Free, data.Referral], (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+};
+
+const processCsv = () => {
+    return new Promise((resolve, reject) => {
+        csv
+            .parseStream(stream, { headers: true })
+            .on('data', async (data) => {
+                console.log(data);
+                try {
+                    await processData(data);
+                } catch (err) {
+                    console.error('Error inserting data:', err.message);
+                    reject(err);
+                }
+            })
+            .on('end', () => {
+                console.log('CSV file successfully processed');
+                resolve();
+            });
+    });
+};
+
+// Call the function to process the CSV data
+processCsv()
+    .then(() => {
+        // Close the database connection after successful insertion
+        db.close();
+    })
+    .catch((err) => {
+        console.error('Error processing CSV:', err.message);
+        db.close();
+    });
---
+++ backend/src/server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import sqlite from 'sqlite3';
+const app = express();
+const port = 3000;
+const sqlite3 = sqlite.verbose();
+
+// Connect to the SQLite database
+const db = new sqlite3.Database('resources.db', (err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.message);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+
+// API endpoint to fetch items
+app.get('/api/resources', (req, res) => {
+  // Retrieve items from the SQLite table
+  db.all('SELECT * FROM resources', (err, rows) => {
+    if (err) {
+      console.error('Error fetching data:', err.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+// API endpoint for searching
+app.get('/api/search', (req, res) => {
+  const searchTerm = req.query.term;
+  const query = `SELECT * FROM resources WHERE name LIKE '%${searchTerm}%'`;
+
+  db.all(query, (err, rows) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
